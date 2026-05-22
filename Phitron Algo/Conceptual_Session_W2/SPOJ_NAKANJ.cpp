9be4639f1222c7f8df15e91d84653586828b1f07
minhajmojumder69 <minhajmojumder735@gmail.com>
2026-05-22 22:44:08 +0600

    Solving a new Problem

---
+++ Phitron Algo/Conceptual_Session_W2/SPOJ_NAKANJ.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool visited[64][64];
+char grid[64][64];
+int level[64][64];
+vector<pair<int, int>> op = {{-2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {-1, -2}, {1, -2}, {2, -1}, {2, 1}};
+int n, m;
+
+bool valid(int i, int j)
+{
+    if (i < 1 || i > n || j < 1 || j > m)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void bfs(int si, int sj)
+{
+    queue<pair<int, int>> q;
+    q.push({si, sj});
+    visited[si][sj] = true;
+    level[si][sj] = 0;
+    while (!q.empty())
+    {
+        pair<int, int> parent = q.front();
+        q.pop();
+        int parent_i = parent.first;
+        int parent_j = parent.second;
+
+        for (int i = 0; i < 8; i++)
+        {
+            int ci, cj;
+            ci = parent_i + op[i].first;
+            cj = parent_j + op[i].second;
+
+            if (valid(ci, cj) && !visited[ci][cj])
+            {
+                q.push({ci, cj});
+                visited[ci][cj] = true;
+                level[ci][cj] = level[parent_i][parent_j] + 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    n = 8, m = 8;
+
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int si, sj,di,dj;
+        string s,d;
+        cin >> s >> d;
+        si = s[1] - '0';
+        sj = s[0] - 'a' + 1;
+        di = d[1] - '0';
+        dj = d[0] - 'a' +1;
+        memset(visited, false, sizeof(visited));
+        memset(level,-1,sizeof(level));
+        bfs(si, sj);
+        cout << level[di][dj] << endl;
+    }
+
+    return 0;
+}
